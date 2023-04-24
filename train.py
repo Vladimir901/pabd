aa99e291e8813bf3ed47b3ac1ac9405224e184a7
labintsev <andrej.labintsev@yandex.ru>
2023-04-24 20:37:24 +0300

    initial commit

---
+++ train.py
@@ -0,0 +1,16 @@
+# train.py
+
+from tqdm import tqdm
+import time
+
+def run():
+  for i in tqdm(range(10)):
+    time.sleep(1.0)
+    
+  print("Training is done!")
+
+
+if __name__ == "__main__":
+  print("Start training model")
+  run()
+  input()
